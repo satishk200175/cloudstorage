eaea882e7a8ad75001fe19ccbf06a7a25cf488a2
satishkumar <satishk20@outlook.com>
2021-01-21 22:38:23 -0500

    finishing final test cases

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
+import org.openqa.selenium.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -90,6 +91,10 @@ class CloudStorageApplicationTests {
 	@Test
 	public void testNotes() throws Exception{
 
+		/**
+		 *	Perform Sign-up
+		 */
+
 		driver.get("http://localhost:" + this.port + "/signup");
 		Assertions.assertEquals("Sign Up", driver.getTitle());
 		signupPage.doSignup("noteme", "noteme", "noteme", "noteme");
@@ -97,28 +102,111 @@ class CloudStorageApplicationTests {
 		try{
 			error = driver.findElement(By.id("error-msg"));
 		}catch(Exception e){
-			System.out.println("error mssage element not found");
+			System.out.println("User sign-up complete");
 		}
 		Assertions.assertNull(error);
 
+		/**
+		 * 	Perform Login
+		 */
 		driver.get("http://localhost:" + this.port + "/login");
 		Assertions.assertEquals("Login", driver.getTitle());
 		loginPage.performLogin("noteme", "noteme");
-
 		Assertions.assertEquals("Home", driver.getTitle());
 
-
-		//driver.findElement(By.xpath("//a[text()='Notes']")).click();
+		/**
+		 *  Create a New Note
+		 */
+		delayLoad(1000);
 		driver.findElement(By.xpath("//*[@id=\"nav-notes-tab\"]")).click();
-
-		//homePage.addNote();
-
+		delayLoad(1000);
+		homePage.addNote();
+		delayLoad(1000);
 		homePage.submitNote("title1", "description 1");
-
+		delayLoad(1000);
+		driver.findElement(By.xpath("//*[@id=\"nav-notes-tab\"]")).click();
+		delayLoad(1000);
 		Assertions.assertEquals("title1", driver.findElement(By.xpath("//*[@id=\"userTable\"]/tbody/tr/th/span")).getText());
-
 		Assertions.assertEquals("description 1", driver.findElement(By.xpath("//*[@id=\"userTable\"]/tbody/tr/td[2]/span")).getText());
 
+		/**
+		 * 	View the just created Note
+		 */
+		homePage.editNote("title2", "description 2");
+		Assertions.assertEquals("title2", driver.findElement(By.xpath("//*[@id=\"userTable\"]/tbody/tr/th/span")).getText());
+		Assertions.assertEquals("description 2", driver.findElement(By.xpath("//*[@id=\"userTable\"]/tbody/tr/td[2]/span")).getText());
+
+		/**
+		 * 	Delete the just edited Note
+		 */
+		homePage.deleteNote();
+		WebElement titleElement = null;
+		try{
+			titleElement = driver.findElement(By.xpath("//*[@id=\"userTable\"]/tbody/tr/th/span"));
+		} catch(NoSuchElementException e){
+			System.out.println("Element not found - - And its good");
+		}
+		Assertions.assertNull(titleElement);
+
+	}
+
+	@Test
+	public void testCredentials(){
+		/**
+		 *	Perform Sign-up
+		 */
+
+		driver.get("http://localhost:" + this.port + "/signup");
+		Assertions.assertEquals("Sign Up", driver.getTitle());
+		signupPage.doSignup("credme", "credme", "credme", "credme");
+		WebElement error = null;
+		try{
+			error = driver.findElement(By.id("error-msg"));
+		}catch(Exception e){
+			System.out.println("User sign-up complete");
+		}
+		Assertions.assertNull(error);
+
+		/**
+		 * 	Perform Login
+		 */
+		driver.get("http://localhost:" + this.port + "/login");
+		Assertions.assertEquals("Login", driver.getTitle());
+		loginPage.performLogin("credme", "credme");
+		Assertions.assertEquals("Home", driver.getTitle());
+
+		/**
+		 * 	Create new Credential
+		 */
+		homePage.addNewCredential("www.google.com", "credme", "credme");
+		Assertions.assertEquals("www.google.com", driver.findElement(By.xpath("//*[@id=\"credentialTable\"]/tbody/tr/th/span")).getText());
+		Assertions.assertEquals("credme", driver.findElement(By.xpath("//*[@id=\"credentialTable\"]/tbody/tr/td[2]/span")).getText());
+		Assertions.assertNotNull(driver.findElement(By.xpath("//*[@id=\"credentialTable\"]/tbody/tr/td[3]/span")).getText());
+
+		/**
+		 * 	verify Credentials
+		 */
+		homePage.viewCredential();
+		Assertions.assertEquals("www.google.com", driver.findElement(By.xpath("//*[@id=\"credential-url\"]")).getAttribute("value"));
+		Assertions.assertEquals("credme", driver.findElement(By.id("credential-username")).getAttribute("value"));
+		Assertions.assertEquals("credme", driver.findElement(By.id("credential-password")).getAttribute("value"));
+		homePage.closeCredModelView();
+
+		/**
+		 * 	Delete the just viewed Credential
+		 */
+		homePage.deleteCredential();
+		WebElement urlElement = null;
+		try{
+			urlElement = driver.findElement(By.xpath("//*[@id=\"credentialTable\"]/tbody/tr/th/span"));
+		} catch(NoSuchElementException e){
+			System.out.println("Element not found - - And its good");
+		}
+		Assertions.assertNull(urlElement);
+	}
+
+	private void delayLoad(long millsec) throws Exception{
+		Thread.sleep(millsec);
 	}
 
 }
--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/HomePage.java
@@ -6,13 +6,17 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
+
+    private long DELAY_TIME = 1000;
+
     @FindBy(id = "logout-button")
     private WebElement logoutButton;
 
     @FindBy(id = "upload-button")
     private WebElement uploadButton;
 
-    @FindBy(xpath = "//*[@id=\"nav-notes\"]/button[contains(text(),'+ Add a New Note')]")
+    //@FindBy(xpath = "//*[@id=\"nav-notes\"]/button[contains(text(),'+ Add a New Note')]")
+    @FindBy(id = "add-new-note-button")
     private WebElement addNoteButton;
 
     @FindBy(id = "note-title")
@@ -27,8 +31,75 @@ public class HomePage {
     @FindBy(id = "nav-notes-tab")
     private WebElement notesTab;
 
+    @FindBy(id = "edit-note-button")
+    private WebElement editNoteButton;
+
+    @FindBy(xpath = "//*[@id=\"userTable\"]/tbody/tr/td[1]/a")
+    private WebElement deleteNoteButton;
+
+    @FindBy(id = "nav-credentials-tab")
+    private WebElement credentialsTab;
+
+    @FindBy(id = "add-cred-button")
+    private WebElement addCredButton;
+
+    @FindBy(id = "credential-url")
+    private WebElement inputCredUrl;
+
+    @FindBy(id = "credential-username")
+    private WebElement inputCredUsername;
+
+    @FindBy(id = "credential-password")
+    private WebElement inputCredPassword;
+
+    @FindBy(xpath = "//*[@id=\"credentialModal\"]/div/div/div[3]/button[2]")
+    private WebElement saveCredButton;
+
+    @FindBy(id = "edit-credential")
+    private WebElement editCredButton;
+
+    @FindBy(xpath = "//*[@id=\"credentialModal\"]/div/div/div[3]/button[1]")
+    private WebElement closeCredModelView;
+
+    @FindBy(xpath = "//*[@id=\"credentialTable\"]/tbody/tr/td[1]/a")
+    private WebElement deleteCredButton;
+
     public HomePage(WebDriver driver){
         PageFactory.initElements(driver, this);
+    }
+
+    public void addNewCredential(String url, String username, String password){
+        delayLoad(DELAY_TIME);
+        credentialsTab.click();
+        delayLoad(DELAY_TIME);
+        addCredButton.click();
+        delayLoad(DELAY_TIME);
+        inputCredUrl.sendKeys(url);
+        inputCredUsername.sendKeys(username);
+        inputCredPassword.sendKeys(password);
+        saveCredButton.click();
+        delayLoad(DELAY_TIME);
+        credentialsTab.click();
+        delayLoad(DELAY_TIME);
+    }
+
+    public void viewCredential(){
+        editCredButton.click();
+        delayLoad(DELAY_TIME);
+    }
+
+    public void closeCredModelView(){
+        closeCredModelView.click();
+        delayLoad(DELAY_TIME);
+    }
+
+    public void deleteCredential(){
+        delayLoad(DELAY_TIME);
+        deleteCredButton.click();
+        delayLoad(DELAY_TIME);
+        credentialsTab.click();
+        delayLoad(DELAY_TIME);
+
     }
 
 
@@ -50,5 +121,39 @@ public class HomePage {
         inputNoteDescription.sendKeys(description);
         inputNoteTitle.sendKeys(title);
         noteSubmitButton.click();
+    }
+
+    public void editNote(String title, String description){
+        delayLoad(DELAY_TIME);
+        editNoteButton.click();
+        delayLoad(DELAY_TIME);
+        inputNoteTitle.clear();
+        inputNoteTitle.sendKeys(title);
+        inputNoteDescription.clear();
+        inputNoteDescription.sendKeys(description);
+        noteSubmitButton.click();
+        delayLoad(DELAY_TIME);
+        notesTab.click();
+        delayLoad(DELAY_TIME);
+    }
+
+    public void deleteNote(){
+        delayLoad(DELAY_TIME);
+        notesTab.click();
+        delayLoad(DELAY_TIME);
+        deleteNoteButton.click();
+        delayLoad(DELAY_TIME);
+        notesTab.click();
+        delayLoad(DELAY_TIME);
+    }
+
+    private void delayLoad(long millsec){
+        try{
+            Thread.sleep(millsec);
+        } catch(Exception e){
+            System.out.println("Error sleeping");
+            e.printStackTrace();
+        }
+
     }
 }
